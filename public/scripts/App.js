4fc6543e6e54b367316644acd2fdf1283e06b7f5
JashaadGaines <jagaines29@live.com>
2016-08-29 02:28:43 -0500

    Rendering App using React class component

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import UserAvatar from './UserAvatar';
 import CustomerSubheader from './CustomerSubheader';
 import CustomerList from './CustomerList';
+import {Router, Route, hashHistory } from 'react-router'
 
-const App = () => (
-    <div>
-        <UserAvatar />
-        <CustomerSubheader />
-        <CustomerList url="/api/customers" pollInterval={2000}/>
-    </div>
-);
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <UserAvatar />
+                <CustomerSubheader />
+                <CustomerList url="/api/customers" pollInterval={2000}/>
+            </div>
+        )
+    }
+}
 
 export default App;
