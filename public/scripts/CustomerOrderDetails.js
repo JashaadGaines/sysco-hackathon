67c94ea33d538fbce74e0e3086c281386b4d84f4
JashaadGaines <jagaines29@live.com>
2016-08-29 09:26:53 -0500

    Added back link

--- public/scripts/CustomerOrderDetails.js
+++ public/scripts/CustomerOrderDetails.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const CustomerOrderDetails = () => (
     <div>
+        <Link to="/">Back</Link>
         ORDER DETAILS PAGE
     </div>
 )
