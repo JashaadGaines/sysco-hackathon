a677bcb47a4994eb592c6501ad76216fe9dd40cc
JashaadGaines <jagaines29@live.com>
2016-08-29 09:30:12 -0500

    Changed const CustomerOrderDetails to class

--- public/scripts/CustomerOrderDetails.js
+++ public/scripts/CustomerOrderDetails.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-const CustomerOrderDetails = () => (
-    <div>
-        <Link to="/">Back</Link>
-        ORDER DETAILS PAGE
-    </div>
-)
+class CustomerOrderDetails extends React.Component {
+       render(){
+           return (<div>
+                   <Link to="/">Back</Link>
+                   ORDER DETAILS PAGE
+               </div>
+           )
+       }
+}
+
+
 
 
 export default CustomerOrderDetails;
